6fa2a878550131dba024d39a60f549f60c6b0b6f
Md. Mainul Hasan <mdrifatbd5@gmail.com>
2024-11-10 16:27:52 +0600

    remove the console

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const port = 5000;
 const app = express();
 
 app.get("/heavy", (req, res) => {
-    console.log('Heavy request received');
   let total = 0;
-  for (let i = 0; i < 5_000_000; i++) {
+  for (let i = 0; i < 50000000; i++) {
     total++;
   }
   res.send(`The result of the CPU intensive task is ${total}\n`);
