e92755a30bd5962e74c13be7d5ba175131217734
Md. Mainul Hasan <mdrifatbd5@gmail.com>
2024-11-10 17:16:41 +0600

    rate limited

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from "express";
-
+import { rateLimit } from 'express-rate-limit'
 const port = 5000;
 const app = express();
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  message: 'Ar dite parba na monu',
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+
+app.use(limiter)
 
 app.get("/heavy", (req, res) => {
   let total = 0;
